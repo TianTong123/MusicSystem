cc2d317dbd3ced0fdfa25ca08a9866c7a100ca55
林圣东 <497291309@qq.com>
2020-04-03 14:59:48 +0800

    环境更改

--- src/server/env.js
+++ src/server/env.js
@@ -1,6 +1,6 @@
 const apienv = {
   dev: {
-    baseUrl: "http://192.168.17.126:8848/tiantong/",
+    baseUrl: "http://39.107.123.212:8848/tiantong/",
     //testUrl:  'http://192.168.17.126:8090/',
   },
 };
